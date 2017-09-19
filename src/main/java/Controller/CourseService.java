d99e188ddf844d76adca75080ff5f39b93f39f7b
Roovers,Jeroen J <jeroen.roovers@student.fontys.nl>
2017-09-19 11:38:50 +0200

    Services updated

--- src/main/java/Controller/CourseService.java
+++ src/main/java/Controller/CourseService.java
@@ -7,6 +7,7 @@ package Controller;
 
 import InfoSupportWeb.utility.CourseDAOUtils;
 import Model.Course;
+import java.util.List;
 import javax.ejb.Stateless;
 
 /**
@@ -17,25 +18,27 @@ import javax.ejb.Stateless;
 public class CourseService {
 
     private CourseDAOUtils courseDAOUtils;
-    
+
     public CourseService() {
         this.courseDAOUtils = new CourseDAOUtils();
     }
 
     /**
      * Persists a new course with just the bare minimals.
+     *
      * @param name
      * @param code
      * @return course with ID given by database.
      */
     public Course addCourse(String code, String name) {
-        Course c = new Course(name, code);        
-        return courseDAOUtils.addCourse(c);        
+        Course c = new Course(name, code);
+        return courseDAOUtils.addCourse(c);
     }
-    
-        /**
-     * Persists a course.
-     * Persists a new course by readying all the values from a given course.
+
+    /**
+     * Persists a course. Persists a new course by readying all the values from
+     * a given course.
+     *
      * @param course exisiting course to copy
      * @return course with ID given by database.
      */
@@ -44,9 +47,19 @@ public class CourseService {
     }
 
     /**
+     * Gets all courses from the database,
+     *
+     * @return list of courses
+     */
+    public List<Course> getAllCourses() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
      * Edits an existing course
+     *
      * @param course
-     * @return 
+     * @return
      */
     public boolean editCourse(Course course) {
         throw new UnsupportedOperationException("Not yet implemented.");
@@ -54,8 +67,9 @@ public class CourseService {
 
     /**
      * Removes an exisiting course
+     *
      * @param course
-     * @return 
+     * @return
      */
     public boolean removeCourse(Course course) {
         throw new UnsupportedOperationException("Not yet implemented.");
