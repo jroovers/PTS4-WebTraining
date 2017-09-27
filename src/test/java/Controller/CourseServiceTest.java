838b0ac8855b34b64310f2ab7562e32a0082ddf6
I334389 <j.vaes@student.fontys.nl>
2017-09-27 10:06:18 +0100

    Unit tests courseService en LessonService

---
+++ src/test/java/Controller/CourseServiceTest.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import InfoSupportWeb.utility.CourseDAOUtils;
+import Model.Course;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorian
+ */
+public class CourseServiceTest {
+
+    CourseService courseService = new CourseService();
+    private Course c1;
+    private Course c2;
+
+       
+    @Before
+    public void setUp() {
+        String[] testArray = new String[2];
+        testArray[0] = "Jemam";
+        testArray[1] = "Jepap";
+        c1 = new Course(33, "UnitTest1", "name", "textDescription", testArray, "courseMaterials", testArray, 3, 1.1);
+        c2 = new Course(33, "UnitTest1", "name", "textDescription", testArray, "courseMaterials", testArray, 3, 1.1);
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addCourse method, of class CourseService.
+     */
+    @Test
+    public void testAddCourse_String_String() throws Exception {
+        System.out.println("addCourse with code and name");
+        
+        Course expResult = c2;
+        Course result = courseService.addCourse(c1.getCode(), c1.getName());
+        
+        //Test of de course een id heeft gekregen in de database.
+        assertNotNull(result.getId());
+        
+        //Test of de codes en namen gelijk zijn.
+        assertEquals(expResult.getCode(), result.getCode());
+        assertEquals(expResult.getName(), result.getName());
+        
+        courseService.removeCourse(result.getId());
+    }
+
+    /**
+     * Test of addCourse method, of class CourseService.
+     */
+    @Test
+    public void testAddCourse_Course() throws Exception {
+        System.out.println("addCourse");
+        Course expResult = c2;
+        Course result = courseService.addCourse(c1);
+        assertEquals(expResult.getCode(), result.getCode());
+        assertEquals(expResult.getName(), result.getName());
+        assertEquals(expResult.getDescription(), result.getDescription());
+        Assert.assertArrayEquals(expResult.getPriorKnowledge(), result.getPriorKnowledge());
+        assertEquals(expResult.getCourseMaterials(), result.getCourseMaterials());
+        Assert.assertArrayEquals(expResult.getKeyWords(), result.getKeyWords());
+        assertEquals(expResult.getDurationInDays(), result.getDurationInDays());
+        assertEquals(expResult.getCost(), result.getCost(), 0.00);
+        
+        courseService.removeCourse(result.getId());
+    }
+
+    /**
+     * Test of getAllCourses method, of class CourseService.
+     */
+    @Test
+    public void testGetAllCourses() throws Exception {
+        System.out.println("getAllCourses");
+        List<Course> result = courseService.getAllCourses();
+        assertTrue(!result.isEmpty());
+    }
+
+    /**
+     * Test of editCourse method, of class CourseService.
+     */
+    @Test
+    public void testEditCourse() throws Exception {
+        System.out.println("editCourse");
+        boolean expResult = true;
+        boolean result = courseService.editCourse(c1);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of removeCourse method, of class CourseService.
+     */
+    @Test
+    public void testRemoveCourse() throws Exception {
+        System.out.println("removeCourse");
+        boolean expResult = true;
+        Course c3 = courseService.addCourse(c1);
+        boolean result = courseService.removeCourse(c3.getId());
+        assertEquals(expResult, result);
+    }
+    
+}
