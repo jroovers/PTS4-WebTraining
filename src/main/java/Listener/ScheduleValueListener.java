513773d53d4fe830741a3edbd9fedbb892b31a21
Jowelle Klomp <jowelle_klomp@hotmail.com>
2017-10-25 12:03:07 +0200

    Geselecteerde course info weergeven. Button doet nog niet wat hij moet doen (alles null bij click)

---
+++ src/main/java/Listener/ScheduleValueListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listener;
+
+import View.Pages.ScheduleBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ValueChangeListener;
+
+/**
+ *
+ * @author Jowelle
+ */
+public class ScheduleValueListener implements ValueChangeListener {
+
+    @Override
+    public void processValueChange(ValueChangeEvent event)
+            throws AbortProcessingException {
+
+        //access country bean directly
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ScheduleBean scheduleBean = (ScheduleBean) facesContext.getApplication().createValueBinding("#{scheduleBean}").getValue(facesContext);
+        
+        scheduleBean.setCourseID(Long.valueOf(scheduleBean.getCourseID()));
+    }
+    
+}
