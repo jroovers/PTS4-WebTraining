c550a61e8c186d5f4cb02091dfe86ef8e096b89a
Ricardo van Dijke <ricardo.van.dijke@prodrive-technologies.com>
2017-10-25 15:30:43 +0200

    fixed create script and added script with dummy data

--- src/main/database script/Create.sql
+++ src/main/database script/Create.sql
@@ -14,42 +14,45 @@ DROP TABLE IF EXISTS `db_dev_infosupport`.`UserType`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `db_dev_infosupport`.`UserType` (
-  `ID_UserType` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ID_UserType`),
-  UNIQUE INDEX `UserType_UNIQUE` (`Name` ASC)
+	`ID_UserType` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255) NOT NULL,
+	
+	PRIMARY KEY (`ID_UserType`),
+	UNIQUE INDEX `UserType_UNIQUE` (`Name` ASC)
 );
 
 CREATE TABLE `db_dev_infosupport`.`Category` (
-  `ID_Category` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ID_Category`),
-  UNIQUE INDEX `Course_Category_UNIQUE` (`Name` ASC)
+	`ID_Category` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255) NOT NULL,
+	
+	PRIMARY KEY (`ID_Category`),
+	UNIQUE INDEX `Course_Category_UNIQUE` (`Name` ASC)
 );
 
 CREATE TABLE `db_dev_infosupport`.`Course`(
-  `ID_Course` INT NOT NULL AUTO_INCREMENT,
-  `Code` VARCHAR(10) NOT NULL,
-  `Name` VARCHAR(255) NOT NULL,
-  `Description` TEXT,
-  `CourseMaterials` TEXT,
-  `KeyWords` TEXT,
-  `Duration` INT DEFAULT '1',
-  `Cost` DECIMAL,
+	`ID_Course` INT NOT NULL AUTO_INCREMENT,
+	`Code` VARCHAR(10) NOT NULL,
+	`Name` VARCHAR(255) NOT NULL,
+	`Description` TEXT,
+	`CourseMaterials` TEXT,
+	`KeyWords` TEXT,
+	`Duration` INT DEFAULT '1',
+	`Cost` DECIMAL,
   PRIMARY KEY (`ID_Course`)
 );
 
 CREATE TABLE `db_dev_infosupport`.`Course_Category`(
-  `ID_Course` INT NOT NULL,
-  `ID_Category` INT NOT NULL,
-  CONSTRAINT `fk_Category_Course`
-   FOREIGN KEY (`ID_Course`)
-   REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
-   ON DELETE CASCADE,
-  CONSTRAINT `fk_Category_Category`
-   FOREIGN KEY (`ID_Category`)
-   REFERENCES `db_dev_infosupport`.`Category` (`ID_Category`)
-   ON DELETE CASCADE
+	`ID_Course` INT NOT NULL,
+	`ID_Category` INT NOT NULL,
+  
+	CONSTRAINT `fk_Category_Course`
+	FOREIGN KEY (`ID_Course`)
+	REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
+	ON DELETE CASCADE,
+	CONSTRAINT `fk_Category_Category`
+	FOREIGN KEY (`ID_Category`)
+	REFERENCES `db_dev_infosupport`.`Category` (`ID_Category`)
+	ON DELETE CASCADE
 );
 
 CREATE TABLE `db_dev_infosupport`.`User`(
@@ -57,79 +60,82 @@ CREATE TABLE `db_dev_infosupport`.`User`(
 	`Username` VARCHAR(255) NOT NULL,
 	`Password` VARCHAR(255) NOT NULL,
 	`UserType` VARCHAR(255) NOT NULL,
-	 CONSTRAINT `fk_User_Usertype`
-   FOREIGN KEY (`UserType`)
-   REFERENCES `db_dev_infosupport`.`UserType` (`Name`)
-   ON DELETE CASCADE,
-	PRIMARY KEY (`ID_User`)
-)
+	
+	PRIMARY KEY (`ID_User`),
+	CONSTRAINT `fk_User_Usertype`
+	FOREIGN KEY (`UserType`)
+	REFERENCES `db_dev_infosupport`.`UserType` (`Name`)
+	ON DELETE CASCADE
+);
 
 CREATE TABLE `db_dev_infosupport`.`Lesson`(
-  `ID_Lesson` INT NOT NULL AUTO_INCREMENT,
-  `StartTime` DATE NOT NULL,
-  `EndTime` DATE NOT NULL,
-  `Location` VARCHAR(30) NOT NULL,
-  `ID_Course` INT NOT NULL,
+	`ID_Lesson` INT NOT NULL AUTO_INCREMENT,
+	`StartTime` DATE NOT NULL,
+	`EndTime` DATE NOT NULL,
+	`Location` VARCHAR(30) NOT NULL,
+	`ID_Course` INT NOT NULL,
   
-  PRIMARY KEY (`ID_Lesson`),
-  CONSTRAINT `fk_Lesson_ID_Course`
-  FOREIGN KEY (`ID_Course`)
-  REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
-  ON DELETE CASCADE
+	PRIMARY KEY (`ID_Lesson`),
+	CONSTRAINT `fk_Lesson_ID_Course`
+	FOREIGN KEY (`ID_Course`)
+	REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
+	ON DELETE CASCADE
 );
 
 CREATE TABLE `db_dev_infosupport`.`Location` (
-  `ID_Location` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ID_Location`),
-  CONSTRAINT Location_UNIQUE UNIQUE (Name)
+	`ID_Location` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255) NOT NULL,
+  
+	PRIMARY KEY (`ID_Location`),
+	CONSTRAINT Location_UNIQUE UNIQUE (Name)
 );
 
 CREATE TABLE `db_dev_infosupport`.`Registration`(
-  `ID_User` INT NOT NULL AUTO_INCREMENT,
-  `FirstName` VARCHAR(255) NOT NULL,
-  `LastName` VARCHAR(255) NOT NULL,
-  `Email` VARCHAR(30) NOT NULL,
-  `PhoneNumber` INT NOT NULL,
+	`ID_User` INT NOT NULL AUTO_INCREMENT,
+	`FirstName` VARCHAR(255) NOT NULL,
+	`LastName` VARCHAR(255) NOT NULL,
+	`Email` VARCHAR(30) NOT NULL,
+	`PhoneNumber` INT NOT NULL,
   
-  PRIMARY KEY (`ID_User`)
+	PRIMARY KEY (`ID_User`)
 );
 
 CREATE TABLE `db_dev_infosupport`.`Course_TargetUsers`(
-  `ID_Course` INT NOT NULL,
-  `ID_UserType` INT NOT NULL,
-  CONSTRAINT `fk_TargerUsers_Course`
-   FOREIGN KEY (`ID_Course`)
-   REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
-   ON DELETE CASCADE,
-  CONSTRAINT `fk_TargerUsers_UserType`
-   FOREIGN KEY (`ID_UserType`)
-   REFERENCES `db_dev_infosupport`.`UserType` (`ID_UserType`)
-   ON DELETE CASCADE
+	`ID_Course` INT NOT NULL,
+	`ID_UserType` INT NOT NULL,
+	
+	CONSTRAINT `fk_TargerUsers_Course`
+	FOREIGN KEY (`ID_Course`)
+	REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
+	ON DELETE CASCADE,
+	CONSTRAINT `fk_TargerUsers_UserType`
+	FOREIGN KEY (`ID_UserType`)
+	REFERENCES `db_dev_infosupport`.`UserType` (`ID_UserType`)
+	ON DELETE CASCADE
 );
 
 CREATE TABLE `db_dev_infosupport`.`Course_PriorKnowledge`(
-  `ID_Course` INT NOT NULL,
-  `PriorCourse` INT NOT NULL,
-  CONSTRAINT `fk_PriorKnowledge_MainCourse`
-  FOREIGN KEY (`ID_Course`)
-  REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
-  ON DELETE CASCADE,
-CONSTRAINT `fk_PriorKnowledge_PreCourse`
-  FOREIGN KEY (`PriorCourse`)
-  REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
-  ON DELETE CASCADE
+	`ID_Course` INT NOT NULL,
+	`PriorCourse` INT NOT NULL,
+	CONSTRAINT `fk_PriorKnowledge_MainCourse`
+	FOREIGN KEY (`ID_Course`)
+	REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
+	ON DELETE CASCADE,
+	CONSTRAINT `fk_PriorKnowledge_PreCourse`
+	FOREIGN KEY (`PriorCourse`)
+	REFERENCES `db_dev_infosupport`.`Course` (`ID_Course`)
+	ON DELETE CASCADE
 );
 
 CREATE TABLE `db_dev_infosupport`.`Lesson_Registration`(
-  `ID_Lesson` INT NOT NULL,
-  `ID_User` INT NOT NULL,
-  CONSTRAINT `fk_Registration_Lesson`
-   FOREIGN KEY (`ID_Lesson`)
-   REFERENCES `db_dev_infosupport`.`Lesson` (`ID_Lesson`)
-   ON DELETE CASCADE,
-  CONSTRAINT `fk_Registration_User`
-   FOREIGN KEY (`ID_User`)
-   REFERENCES `db_dev_infosupport`.`Registration` (`ID_User`)
-   ON DELETE CASCADE
+	`ID_Lesson` INT NOT NULL,
+	`ID_User` INT NOT NULL,
+	CONSTRAINT `fk_Registration_Lesson`
+	FOREIGN KEY (`ID_Lesson`)
+	REFERENCES `db_dev_infosupport`.`Lesson` (`ID_Lesson`)
+	ON DELETE CASCADE,
+	CONSTRAINT `fk_Registration_User`
+	FOREIGN KEY (`ID_User`)
+	REFERENCES `db_dev_infosupport`.`Registration` (`ID_User`)
+	ON DELETE CASCADE
 );
