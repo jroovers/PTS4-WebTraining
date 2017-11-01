b98f8e1d0478b84e970bbaa5984ddaf108084f5f
kyle_ <kyle_@DESKTOP-A89DD6G>
2017-11-01 15:11:53 +0100

    Login finalfinalfinalfinalfinal

--- src/main/java/View/Pages/AuthorizationBean.java
+++ src/main/java/View/Pages/AuthorizationBean.java
@@ -31,6 +31,7 @@ public class AuthorizationBean {
     private User user;
 
     private String username;
+    private String password;
     private String role;
 
     
@@ -44,28 +45,35 @@ public class AuthorizationBean {
     
 
     public String login() {
-//        FacesContext context = FacesContext.getCurrentInstance();
-//        if (username.isEmpty()) {
-//            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Geen gebruikersnaam ingevoerd.", "!"));
-//        } else {
-//            try {
-//                user = userdb.getUser(username);
-//            } catch (NullPointerException ex) {
-//                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "De gebruikersnaam bestaat niet.", "!"));
-//                return;
-//            }
+        
+        if(username != null && password != null) {
+            userdb = new UserDAOUtils();
+            user = userdb.getUser(username);
+            
+            if(user.getPassword().equals(password)) {
+                HttpSession hs = AuthorizationUtils.getSession();
+                hs.setAttribute("AccesLevel", user.getAccesLevel());
+                hs.setAttribute("Username", user.getUsername());
+                return "/index_templated.xhtml";
+            }
+        }
+        
+        // Gebruiker    : Bertster
+        // Wachtwoord   : bertisthebest
+        // Level        : 1
+        
+        // Gebruiker    : Frankster
+        // Wachtwoord   : frankisthebest
+        // Level        : 
+        
+//            User user = new User(1, "Frank" , "franken", "Frankster", "frankisthebest", "001234", "Frankster@TheG.com", 2);
 //            HttpSession hs = AuthorizationUtils.getSession();
-//            hs.setAttribute("Username", username);
-//            hs.setAttribute("Role", role);
-//            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Ingelogd", "!"));
-//        }
+//            //hs.setAttribute("LoggedInUser", user);
+//            hs.setAttribute("AccesLevel", 2);
+//            hs.setAttribute("Username", "Kyle");
+//            return "/index_templated.xhtml";
 
-            User user = new User(1, "Frank" , "franken", "Frankster", "frankisthebest", "001234", "Frankster@TheG.com", 2);
-            HttpSession hs = AuthorizationUtils.getSession();
-            //hs.setAttribute("LoggedInUser", user);
-            hs.setAttribute("AccesLevel", 2);
-            hs.setAttribute("Username", "Kyle");
-            return "/index_templated.xhtml";
+        return "/authorization.xhtml";
     }
     
     public String logout() {
@@ -121,4 +129,22 @@ public class AuthorizationBean {
             return false;
         }
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
 }
