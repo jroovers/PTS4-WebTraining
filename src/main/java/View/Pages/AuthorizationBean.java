cee6f3d0723549eeaf057355a7f3fe14b7642a2f
kyle_ <kyle_@DESKTOP-A89DD6G>
2017-11-01 10:28:54 +0100

    Authorization

--- src/main/java/View/Pages/AuthorizationBean.java
+++ src/main/java/View/Pages/AuthorizationBean.java
@@ -9,11 +9,14 @@ package View.Pages;
 import InfoSupportWeb.utility.AuthorizationUtils;
 import InfoSupportWeb.utility.UserDAOUtils;
 import Model.User;
+import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.servlet.http.HttpSession;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -40,7 +43,7 @@ public class AuthorizationBean {
     
     
 
-    public void login() {
+    public String login() {
 //        FacesContext context = FacesContext.getCurrentInstance();
 //        if (username.isEmpty()) {
 //            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Geen gebruikersnaam ingevoerd.", "!"));
@@ -61,6 +64,30 @@ public class AuthorizationBean {
             HttpSession hs = AuthorizationUtils.getSession();
             hs.setAttribute("LoggedInUser", user);
             System.out.println("Sessie gestart van: " + user.getUsername());
+            return "/index_templated.xhtml";
     }
-
+    
+    public String logout() {
+        HttpSession hs = AuthorizationUtils.getSession();
+        hs.invalidate();
+        return "/authorization.xhtml";
+    }
+    
+    // 1 admin
+    // 2 docent
+    // 3 cursist
+    public void checkPermission(int accesLevel) throws IOException {
+        HttpSession hs = AuthorizationUtils.getSession();
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        if(hs.getAttribute("LoggedInUser") != null) {
+            user = (User) hs.getAttribute("LoggedInUser");
+            
+            
+            if(user.getAccesLevel() > accesLevel) {
+               context.redirect("/InfoSupportWeb/external/authorization.xhtml"); 
+            }    
+        } else {
+            context.redirect("/InfoSupportWeb/external/authorization.xhtml"); 
+        }
+    }
 }
