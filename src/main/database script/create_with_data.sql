8232ebb3d99bf2215eef76a70896e317786f2984
Jorian <i334389@student.fontys.nl>
2017-11-08 12:01:49 +0100

    Fix SQL

--- src/main/database script/create_with_data.sql
+++ src/main/database script/create_with_data.sql
@@ -30,25 +30,6 @@ INSERT INTO `Category` (Name) VALUES
 	('Office'),
 	('Sql');
 
-	-- Dumping structure for table db_dev_infosupport.Category
-DROP TABLE IF EXISTS `UserGroup`;
-CREATE TABLE IF NOT EXISTS `UserGroup` (
-  `ID_UserGroup` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID_UserGroup`),
-  UNIQUE KEY `UserGroup_UNIQUE` (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table db_dev_infosupport.Category: ~0 rows (approximately)
-/*!40000 ALTER TABLE `Category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Category` ENABLE KEYS */;
-
-INSERT INTO `UserGroup` (Name) VALUES
-	('Instroom 2017'),
-	('MDWK Basiskennis'),
-	('MDWK Nieuwe Medewerkers'),
-	('MDWK Senior Medewerkers');
-
 -- Dumping structure for table db_dev_infosupport.Course
 DROP TABLE IF EXISTS `Course`;
 CREATE TABLE IF NOT EXISTS `Course` (
@@ -108,14 +89,14 @@ CREATE TABLE IF NOT EXISTS `Course_PriorKnowledge` (
 /*!40000 ALTER TABLE `Course_PriorKnowledge` ENABLE KEYS */;
 
 -- Dumping structure for table db_dev_infosupport.Course_TargetUsers
-DROP TABLE IF EXISTS `Course_UserGroup`;
-CREATE TABLE IF NOT EXISTS `Course_UserGroup` (
+DROP TABLE IF EXISTS `Course_TargetUsers`;
+CREATE TABLE IF NOT EXISTS `Course_TargetUsers` (
   `ID_Course` int(11) NOT NULL,
-  `ID_UserGroup` int(11) NOT NULL,
+  `ID_UserType` int(11) NOT NULL,
   KEY `fk_TargerUsers_Course` (`ID_Course`),
-  KEY `fk_TargerUsers_UserGroup` (`ID_UserGroup`),
-  CONSTRAINT `fk_TargetUsers_Course` FOREIGN KEY (`ID_Course`) REFERENCES `Course` (`ID_Course`) ON DELETE CASCADE,
-  CONSTRAINT `fk_TargerUsers_UserGroup` FOREIGN KEY (`ID_UserGroup`) REFERENCES `UserGroup` (`ID_UserGroup`) ON DELETE CASCADE
+  KEY `fk_TargerUsers_UserType` (`ID_UserType`),
+  CONSTRAINT `fk_TargerUsers_Course` FOREIGN KEY (`ID_Course`) REFERENCES `Course` (`ID_Course`) ON DELETE CASCADE,
+  CONSTRAINT `fk_TargerUsers_UserType` FOREIGN KEY (`ID_UserType`) REFERENCES `UserType` (`ID_UserType`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Dumping data for table db_dev_infosupport.Course_TargetUsers: ~0 rows (approximately)
@@ -456,7 +437,7 @@ INSERT INTO `User` (`ID_User`, `Name`, `Surname`, `Username`, `Password`,`PhoneN
 	(1, 'Frank', 'franken', 'Frankster', 'frankisthebest', '001234', 'Frankster@TheG.com', 2),
 	(2, 'Bert', 'bertus','Bertster','bertisthebest','004321','BertusThebertustest@banana.com', 1),
 	(3, 'Kyle', 'bendover','Bender','BendingLoverxx','09000900','Kylethe****Lover@gmail.com', 3),
-	(3, 'Bart', 'nobendover','Bender','BendingLoverxx','09000900','KyleTheWeab@msn.com', 4);
+	(4, 'Bart', 'nobendover','Appeltje','banaan','004312321','KyleTheWeab@msn.com', 4);
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
