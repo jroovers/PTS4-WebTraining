f5badfcf3766c6a16fc8805ee869ad227908aaaa
kyle_ <kyle_@DESKTOP-A89DD6G>
2017-11-21 11:43:33 +0100

    MOOOI

--- src/main/java/View/Pages/AuthorizationBean.java
+++ src/main/java/View/Pages/AuthorizationBean.java
@@ -5,7 +5,6 @@
  */
 package View.Pages;
 
-
 import InfoSupportWeb.utility.AuthorizationUtils;
 import InfoSupportWeb.utility.UserDAOUtils;
 import Model.User;
@@ -20,7 +19,6 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
-
 /**
  *
  * @author kyle_
@@ -34,40 +32,30 @@ public class AuthorizationBean {
 
     private String username;
     private String password;
-    private String role;
-    
-    private List course_page = new ArrayList<>() ;
-    private List panner_page = new ArrayList();
-    private List personalroster_page = new ArrayList();
-    private List schedule_page = new ArrayList();
-    private List offertraining_page = new ArrayList();
-    private List signup_page = new ArrayList();
 
     public AuthorizationBean() {
         username = "";
-        role = "werknemer";
     }
-    
-     public String logindummie() {
-         List<Integer> acceslist = new ArrayList<Integer>();
-         acceslist.add(1);
-         acceslist.add(2);
-         HttpSession hs = AuthorizationUtils.getSession();
-         hs.setAttribute("UserID", 1);
-         hs.setAttribute("AccesLevels", acceslist);
-         hs.setAttribute("Username", "Kyle");
-         return "/index_templated.xhtml";
-         
-     }
-    
+
+    public String logindummie() {
+        List<Integer> acceslist = new ArrayList<Integer>();
+        acceslist.add(1);
+        acceslist.add(2);
+        HttpSession hs = AuthorizationUtils.getSession();
+        hs.setAttribute("UserID", 1);
+        hs.setAttribute("AccesLevels", acceslist);
+        hs.setAttribute("Username", "Kyle");
+        return "/index_templated.xhtml";
+
+    }
 
     public String login() {
-        
-        if(username != null && password != null) {
+
+        if (username != null && password != null) {
             userdb = new UserDAOUtils();
             user = userdb.getUser(username);
-            
-            if(user.getPassword().equals(password)) {
+
+            if (user.getPassword().equals(password)) {
                 HttpSession hs = AuthorizationUtils.getSession();
                 hs.setAttribute("UserID", user.getUserID());
                 hs.setAttribute("AccesLevels", user.getAccesLevels());
@@ -75,7 +63,7 @@ public class AuthorizationBean {
                 return "/index_templated.xhtml";
             }
         }
-        
+
         // Gebruiker    : Bertster
         // Wachtwoord   : bertisthebest
         // Level        : 1
@@ -83,42 +71,16 @@ public class AuthorizationBean {
         // Gebruiker    : Frankster
         // Wachtwoord   : frankisthebest
         // Level        : 2
-
+        
         return "/authorization.xhtml";
     }
-    
+
     public String logout() {
         HttpSession hs = AuthorizationUtils.getSession();
         hs.invalidate();
         return "/index_templated.xhtml";
     }
-    
-    
-//    /**
-//     * Checks if the user has the permission to use the page.
-//     * @param accesLevel Level that the user has acces to.
-//     * @throws IOException 
-//     */
-//    public void checkPermission(int accesLevel) throws IOException {
-//        HttpSession hs = AuthorizationUtils.getSession();
-//        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-//        try {
-//            int level = (int) hs.getAttribute("AccesLevel");
-//
-//            if (accesLevel == 1) {
-//                return;
-//            }
-//
-//            if (level >= accesLevel) {
-//                return;
-//            }
-//            context.redirect("/InfoSupportWeb/external/authorization.xhtml");
-//        } catch (NullPointerException ex) {
-//            context.redirect("/InfoSupportWeb/external/authorization.xhtml");
-//        }
-//    }
-    
-    
+
     public void checkPermission(int accesLevel) throws IOException {
         HttpSession hs = AuthorizationUtils.getSession();
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
@@ -133,16 +95,17 @@ public class AuthorizationBean {
                     return;
                 }
             }
-            
-            context.redirect("/InfoSupportWeb/external/authorization.xhtml"); 
-        } catch(Exception ex) {
-            context.redirect("/InfoSupportWeb/external/authorization.xhtml"); 
+
+            context.redirect("/InfoSupportWeb/external/authorization.xhtml");
+        } catch (Exception ex) {
+            context.redirect("/InfoSupportWeb/external/authorization.xhtml");
         }
- 
+
     }
-    
-       /**
+
+    /**
      * Checks if the users acceslevel is high enough.
+     *
      * @param accesLevel
      * @return True if acceslevel is high enough, false if not
      */
@@ -158,36 +121,12 @@ public class AuthorizationBean {
                     return true;
                 }
             }
-            return false;  
+            return false;
         } catch (NullPointerException ex) {
             return false;
         }
     }
-
-//    
-//        /**
-//     * Checks if the users acceslevel is high enough.
-//     * @param accesLevel
-//     * @return True if acceslevel is high enough, false if not
-//     */
-//    public boolean checkVisible(int accesLevel) {
-//        HttpSession hs = AuthorizationUtils.getSession();
-//        try {
-//            int level = (int) hs.getAttribute("AccesLevel");      
-//            
-//            if (accesLevel == 1) {
-//                return true;
-//            }
-//            
-//            if (level >= accesLevel) {
-//                return true;
-//            }
-//            return false;  
-//        } catch (NullPointerException ex) {
-//            return false;
-//        }
-//    }
-
+    
     public String getUsername() {
         return username;
     }
@@ -203,6 +142,5 @@ public class AuthorizationBean {
     public void setPassword(String password) {
         this.password = password;
     }
-    
-    
+
 }
